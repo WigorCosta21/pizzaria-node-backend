b79d14a2b1daab2408184f98d7135263353fa43d
Wigor <wigor21@hotmail.com>
2026-08-15 15:59:44 -0300

    chore: add cors and dotenv

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import "dotenv/config";
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
